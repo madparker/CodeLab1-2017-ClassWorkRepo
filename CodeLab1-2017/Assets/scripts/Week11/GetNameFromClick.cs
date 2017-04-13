9fe2646c5d4a3b4f8399f067431210a8eeabc149
Matthew Parker <maddparker@gmail.com>
2017-04-13 13:56:35 -0400

    Week 11 commit
    Camera, Raycasting, and Rotate Around
    Also added terrian textures

---
+++ CodeLab1-2017/Assets/scripts/Week11/GetNameFromClick.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetNameFromClick : MonoBehaviour {
+
+	private string objectName;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)){ //On mouse button down
+			RaycastHit rayHit; //create a RaycastHit object
+
+			//Call the Physics.Raycast function with Camera world position of the mouse, the rayHit object, and the far clipping plane
+			if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out rayHit, Camera.main.farClipPlane)){
+				Debug.Log(rayHit.collider.name); //Print the name of the object
+			} else {
+				Debug.Log("Hit nothing"); //if you hit nothing, print that
+			}
+		}
+	}
+}
---
+++ CodeLab1-2017/Assets/scripts/Week11/RotateAroundExample.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAroundExample : MonoBehaviour {
+
+	public GameObject target;
+	public Vector3 axis;
+
+	private float angle = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.RotateAround(target.transform.position, axis, angle); //Rotate an object around a point and an axis, by "angle" degrees
+	}
+}
