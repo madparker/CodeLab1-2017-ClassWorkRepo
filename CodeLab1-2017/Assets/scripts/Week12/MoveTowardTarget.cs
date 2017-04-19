e4987a29f57d05f19761203ab934e09763c76376
Matthew Parker <maddparker@gmail.com>
2017-04-19 14:51:10 -0400

    Week 12
    Vector Maths

---
+++ CodeLab1-2017/Assets/scripts/Week12/MoveTowardTarget.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTowardTarget : MonoBehaviour {
+
+	public Transform target;
+	public float speed = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 dir = target.position - transform.position;
+
+//		Debug.DrawRay(transform.position, dir, Color.magenta);
+
+		dir.Normalize();
+//		dir = new Vector3(
+//			dir.x/dir.magnitude,
+//			dir.y/dir.magnitude,
+//			dir.z/dir.magnitude);
+
+
+		Debug.Log("Facing? " + transform.forward);
+
+		Debug.Log("Dir: " + dir);
+		Debug.Log("Normalized dir? " + dir.magnitude);
+
+		if(Vector3.Dot(transform.forward, dir) > 0){
+			Debug.DrawRay(transform.position, dir, Color.cyan);
+
+//			transform.Translate(dir * speed * Time.deltaTime);
+			transform.position += dir * speed * Time.deltaTime;
+
+			transform.rotation = Quaternion.Slerp(transform.rotation,Quaternion.LookRotation(dir), .1f);
+		}
+
+		Debug.DrawRay(transform.position, transform.forward * 3, Color.black);
+		Debug.DrawRay(transform.position, transform.up * 3, Color.black);
+	}
+}
---
+++ CodeLab1-2017/Assets/scripts/Week12/VectorMaths.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VectorMaths : MonoBehaviour {
+
+	public Vector3 startPoint = new Vector3(0,0,0);
+	public Vector3 vec1 = new Vector3(10, 0, 0);
+	public Vector3 vec2 = new Vector3(0, 0, 10);
+
+	public Vector3 inAngle;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Debug.DrawRay(startPoint, vec1, Color.green);
+
+		Debug.Log("Vec1 Mag: " + vec1.magnitude);
+		Debug.Log("Vec1 Normalized:" + Vector3.Normalize(vec1));
+
+		Debug.Log("Dot: " + Vector3.Dot(Vector3.right, Vector3.down));
+
+		Debug.DrawRay(startPoint, vec1, Color.white);
+		Debug.DrawRay(startPoint, vec2, Color.white);
+
+
+		Vector3 cross = Vector3.Cross(vec1, vec2);
+
+		Debug.DrawRay(startPoint, cross, Color.yellow);
+
+		Debug.DrawRay(startPoint, inAngle, Color.red);
+
+		Vector3 reflection = Vector3.Reflect(inAngle, cross.normalized);
+
+		Debug.DrawRay(startPoint, -reflection, Color.blue);
+	}
+}
