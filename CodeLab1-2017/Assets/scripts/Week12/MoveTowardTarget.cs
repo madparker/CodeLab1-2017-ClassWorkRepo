3ac3a10acce4f3d9432aad6d0443109687293753
Matthew Parker <maddparker@gmail.com>
2017-04-19 20:48:52 -0400

    Week 12
    With Comments and side cannons

--- CodeLab1-2017/Assets/scripts/Week12/MoveTowardTarget.cs
+++ CodeLab1-2017/Assets/scripts/Week12/MoveTowardTarget.cs
@@ -14,32 +14,75 @@ public class MoveTowardTarget : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		//Get the direction vector from the current position to the target
 		Vector3 dir = target.position - transform.position;
 
+		//Draw the direction array from the current position 
 //		Debug.DrawRay(transform.position, dir, Color.magenta);
 
+		//Normalize the dir vector
 		dir.Normalize();
+
+		//if we wanted to, we don't need to use Unity's fancy direction function
+		//we can just calculate it ourselves!
 //		dir = new Vector3(
 //			dir.x/dir.magnitude,
 //			dir.y/dir.magnitude,
 //			dir.z/dir.magnitude);
 
 
+		//print the Euler direction the transform is facing
 		Debug.Log("Facing? " + transform.forward);
 
+		//Print the normalized dir
 		Debug.Log("Dir: " + dir);
+
+		//confirm the magnitude of the dir is 1 after it's normalized (spoiler alert: it is)
 		Debug.Log("Normalized dir? " + dir.magnitude);
 
+		//Get the dot product of the "forward" and the dir
+		//The dot product for normalized vectors will be
+		//  1 if the vectors are the same
+		// -1 if the vectors are opposite
+		//  0 if the vectors are perpendicular
+		// >0 if there is less than 90 degrees between the vectors
+		// <0 if there is greater than 90 degrees between the vectors
 		if(Vector3.Dot(transform.forward, dir) > 0){
+			//if the target is in front of the object, draw a line
 			Debug.DrawRay(transform.position, dir, Color.cyan);
 
-//			transform.Translate(dir * speed * Time.deltaTime);
+			//move towards the target
 			transform.position += dir * speed * Time.deltaTime;
 
+			//rotate towards the target
 			transform.rotation = Quaternion.Slerp(transform.rotation,Quaternion.LookRotation(dir), .1f);
 		}
 
+		//Draw a ray coming out of the front of the object
 		Debug.DrawRay(transform.position, transform.forward * 3, Color.black);
+		//Draw a ray coming out of the top of the object
 		Debug.DrawRay(transform.position, transform.up * 3, Color.black);
+
+		//if "space" is pressed
+		if(Input.GetKeyDown(KeyCode.Space)){
+			//Get the cross product of "forward" and "up" for this transform
+			//The cross product will return the "normal" for a plane described
+			//by two vectors. The "normal" is a vector perpendicular to both
+			//vectors describing the ray
+			Vector3 bulletDir = Vector3.Cross(transform.forward, transform.up);
+
+			//Make a bullet
+			GameObject b1 = Instantiate(Resources.Load("Bullet")) as GameObject;
+			//give it velocity in the dir of the side of the object
+			b1.GetComponent<Rigidbody>().velocity = bulletDir * 3;
+			//Make another bullet
+			GameObject b2 = Instantiate(Resources.Load("Bullet")) as GameObject;
+			//give it velocity in the dir of the side of the object
+			b2.GetComponent<Rigidbody>().velocity = bulletDir * -3;
+
+			//Move the bullets to a position slightly offset from the current position
+			b1.transform.position = transform.position + bulletDir;
+			b2.transform.position = transform.position - bulletDir;
+		}
 	}
 }
--- CodeLab1-2017/Assets/scripts/Week12/VectorMaths.cs
+++ CodeLab1-2017/Assets/scripts/Week12/VectorMaths.cs
@@ -18,25 +18,39 @@ public class VectorMaths : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+		//Draw a Debug ray
 		Debug.DrawRay(startPoint, vec1, Color.green);
 
+		//Print out the magnitude of a vector
 		Debug.Log("Vec1 Mag: " + vec1.magnitude);
+		//Print out the normalized version of vector
 		Debug.Log("Vec1 Normalized:" + Vector3.Normalize(vec1));
 
+		//Print out the Dot Product of 2 vectors, in this case 0
+		//(for more info about Dot Products, look at line 43 of
+		//MoveTowardTarget
 		Debug.Log("Dot: " + Vector3.Dot(Vector3.right, Vector3.down));
 
+		//Draw a 2 vectors to describe a plane
 		Debug.DrawRay(startPoint, vec1, Color.white);
 		Debug.DrawRay(startPoint, vec2, Color.white);
 
 
+		//create a cross product of the 2 rays
+		//(for more info about Dot Products, look at line 68 of
+		//MoveTowardTarget
 		Vector3 cross = Vector3.Cross(vec1, vec2);
 
+		//Draw the cross product (normal to the plane)
 		Debug.DrawRay(startPoint, cross, Color.yellow);
 
+		//Draw a vector going into the plane
 		Debug.DrawRay(startPoint, inAngle, Color.red);
 
+		//Get the reflection of inAngle using the normal
 		Vector3 reflection = Vector3.Reflect(inAngle, cross.normalized);
 
+		//Draw the reflection of the inAngle
 		Debug.DrawRay(startPoint, -reflection, Color.blue);
 	}
 }
